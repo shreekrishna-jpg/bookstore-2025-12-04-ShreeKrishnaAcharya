8d10a6a8be68d79ad0680c9aa95a87248e27da9f
Shreekrshna Acharya <acharyashreekrishna96@gmail.com>
2026-01-22 11:25:10 -0600

    Lab1: Rebranding

--- src/main/java/csd214/bookstore/Main.java
+++ src/main/java/csd214/bookstore/Main.java
@@ -2,7 +2,7 @@ package csd214.bookstore;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bookstore Application Started");
+        System.out.println("Shree's Superstore - v1.0");
         new App().run();
     }
 }
